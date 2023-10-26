065c6c9ed0a41372113c910f214c2e728dd1e284
ANDERSON LUIGUI <alns1536@hotmail.com>
2023-10-25 21:35:27 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+// OBJETOS ----- ARRAY DE OBJETOS ----------
 const people = [
     {name: 'AndersonLuigui', code: 123 },
     {name: 'NeuraBatista', code: 426},
     {name: 'JoaoMiguel', code: 555}
 ]
+
+// ASSIM QUE A PÁGINA É CARREGADA 'DOMContentLoaded' É EXECUTADA A FUNÇÃO ONDE ACONTECE AS SEGUINTES AÇÕES;
+// ESCUTA É ADICIONADA NO BOTÃO
+// É OBTIDO O VALOR DO USERNAME E PASSWORD
+// É SELECIONADO UMA DIV NO HTML PARA MENSAGEM DE ERRO errorMessage.textContent
+// CONDIÇÃO FALSE CASO O username E password ESTEJA SEM VALOR
+// CONDIÇÃO TRUE ELSE 
+// DEPOIS INTERAÇÃO for (const user of people) 
+// window.location.href = 'pagina_exclusiva.html' CASO userFound TRUE É INCIALIZADA A PÁGINA EXCLUSIVA
+// OUTRA CONDIÇÃO CASO SEJA DIFERENTE DE userFound MENSAGEM DE ERRO APARECE
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
