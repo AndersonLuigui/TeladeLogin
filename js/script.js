53cb73973f27c52115d2cc4a97ed1679ac34db1f
ANDERSON LUIGUI <alns1536@hotmail.com>
2023-10-27 19:27:45 -0300

    integração DB

--- js/script.js
+++ js/script.js
@@ -23,19 +23,19 @@ document.addEventListener('DOMContentLoaded', function() {
         const username = document.getElementById('username').value;
         const password = parseInt(document.getElementById('password').value);
         const errorMessage = document.getElementById('errorMessage');
+
         errorMessage.textContent = '';
 
-        if (username === '' || password === '') {
-            const errorMessage = document.getElementById('errorMessage');
+        if (username === '' || isNaN(password)) {
             errorMessage.textContent = 'Nome de usuário e senha são obrigatórios';
         } else {
             let userFound = false;
+
             for (const user of people) {
                 if (user.name === username && user.code === password) {
                     alert('Bem vindo, ' + username);
-                
                     window.location.href = '/html/pagina_exclusiva.html';
-                    userFound = true;        
+                    userFound = true;
                     break;
                 }
             }
@@ -44,6 +44,57 @@ document.addEventListener('DOMContentLoaded', function() {
                 errorMessage.textContent = 'Nome de usuário ou senha está incorreto';
             }
         }
-    })
-})
 
+
+        const registerButton = document.getElementById('registerButton');
+        registerButton.addEventListener('click', function() {
+            window.location.href = '/html/register.html';
+        });
+
+
+
+    });
+
+
+
+
+
+
+
+    const registerUserButton = document.getElementById('registerUser');
+        registerUserButton.addEventListener('click', function() {
+            const newUsername = document.getElementById('newUsername').value;
+            const newPassword = parseInt(document.getElementById('newPassword').value);
+            const errorRegister = document.getElementById('errorRegister');
+            const sucessRegister = document.getElementById('sucessRegister');
+
+            errorRegister.textContent = '';
+            sucessRegister.textContent = '';
+
+            if (newUsername === '' || isNaN(newPassword)) {
+                errorRegister.textContent = 'Nome de usuário e senha são obrigatórios';
+
+            } else {
+                const userExists = people.some(user => user.name === newUsername);
+
+                if (userExists) {
+                    errorRegister.textContent = 'Usuário já cadastrado';
+                } else {
+                    people.push({name: newUsername, code: newPassword});
+                    sucessRegister.textContent = ('Usuário cadastrado com sucesso');
+                }
+
+                document.getElementById('newUsername').value = '';
+                document.getElementById('newPassword').value = '';
+                document.getElementById('registerUser').style.display = 'none';
+                document.getElementById('loginButton').addEventListener('click', function(){
+                    window.location.href = '/index.html';
+                })
+
+
+            }
+
+    })
+
+
+});
